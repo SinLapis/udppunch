05ad7e9a529715b39ffb8e4e9a1f72ff540b6675
SinLapis <truthdragon@moegirl.org>
2015-07-07 19:56:44 +0800

    add server

--- client.py
+++ client.py
@@ -1,2 +1,17 @@
+#-*- coding:utf-8 -*-
 import threading, socket
+
+class Client():
+
+	def __init__(self):
+
+		self.sockfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	def start(self):
+
+		self.sockfd.sendto
+
+	def send(self):
+		
+
 

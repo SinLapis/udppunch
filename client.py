c0c1edc56d2a73c8cf291149d1f67dff7a5b1820
SinLapis <truthdragon@moegirl.org>
2015-07-13 20:23:13 +0800

    fixed

--- client.py
+++ client.py
@@ -1,16 +1,19 @@
 #-*- coding:utf-8 -*-
 import threading, socket, re, time
 
-server_addr = ('54.169.131.175', 9999)
+#服务器地址
+server_addr = ('127.0.0.1', 9999) 
 
 class Client():
 
-	def __init__(self):
+	#初始化Client类，实例化socket
+	def __init__(self): 
 
 		self.sockfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.self_addr = None
 		self.other_addr = None
 
+	#从字符串中提取地址并转换成元组
 	def addr_convert(self, s):
 
 		re_addr = re.compile(r'\(\'(.*)\',\s(.*)\)')
@@ -19,6 +22,7 @@ class Client():
 		port = int(t[1])
 		return ip, port
 
+	#提取地址和聊天信息
 	def msg_convert(self, s):
 
 		re_msg = re.compile(r'addr:(.*)msg:(.*)')
@@ -27,6 +31,7 @@ class Client():
 		msg = t[1]
 		return addr, msg
 
+	#向服务器注册信息
 	def start(self):
 
 		while True:
@@ -35,10 +40,12 @@ class Client():
 
 			data, addr = self.sockfd.recvfrom(1024)
 			data = data.decode('utf-8')
+			print('self_addr: %s' % data)
 			if addr == server_addr:
 				self.self_addr = self.addr_convert(data)
 				break
-		
+	
+	#请求对等方地址
 	def request(self):
 
 		while True:
@@ -48,12 +55,14 @@ class Client():
 			data = data.decode('utf-8')
 			if data != 'none' and addr == server_addr:
 				self.other_addr = self.addr_convert(data)
+				print('other_addr: %s' % data)
 				break
 
+	#尝试直接连接
 	def init(self):
 
 		n = 0
-		while n > 10:
+		while n < 10:
 			data = 'init'
 			print('init')
 			self.sockfd.sendto(data.encode('utf-8'), self.other_addr)
@@ -70,6 +79,7 @@ class Client():
 			n += 1
 		return False
 
+	#直接连接的聊天部分
 	def chat(self):
 
 		def send(sock):
@@ -84,11 +94,14 @@ class Client():
 				data, addr = sock.recvfrom(1024)
 				if addr == self.other_addr:
 					data = data.decode('utf-8')
+					print('time:' + str(time.asctime()))
+					print('from:' + str(addr))
 					print(data)
 
 		threading.Thread(target = send, args = (self.sockfd, )).start()
 		threading.Thread(target = recv, args = (self.sockfd, )).start()
 
+	#转发的聊天部分
 	def sy_chat(self):
 
 		def sy_send(sock):
@@ -104,19 +117,20 @@ class Client():
 				data = data.decode('utf-8')
 				try:
 					addr, msg = self.msg_convert(data)
+					print('time:' + time.time())
 					print('from:' + addr + '\n' + msg)
 				except Exception as e:
 					print('error')
-					pass
 
 		threading.Thread(target = sy_send, args = (self.sockfd, )).start()
 		threading.Thread(target = sy_recv, args = (self.sockfd, )).start()
 
-
+	#主函数
 	def main(self):
 		self.start()
 		self.request()
 		is_sy = self.init()
+		print('direct link:')
 		print(is_sy)
 		if is_sy == True:
 			self.chat()
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import threading, socket, re
 
 class Server():
 
+	#初始化服务器参数，实例化socket
 	def __init__(self):
 
 		SERVER_PORT = 9999
@@ -11,6 +12,7 @@ class Server():
 		self.sockfd.bind(('',SERVER_PORT))
 		self.addr_list = []
 
+	#从字符串中提取地址并转换成元组
 	def addr_convert(self, s):
 
 		re_addr = re.compile(r'\(\'(.*)\',\s(.*)\)')
@@ -19,6 +21,7 @@ class Server():
 		port = int(t[1])
 		return ip, port
 
+	#提取地址和聊天信息
 	def msg_convert(self, s):
 
 		re_msg = re.compile(r'addr:(.*)msg:(.*)')
@@ -27,26 +30,32 @@ class Server():
 		msg = t[1]
 		return addr, msg
 
+	#主函数，根据客户端发来的不同信息进行处理
 	def main(self):
 
 		while True:
-			recv_data, addr = self.sockfd.recvfrom(1024)
+			recv_data, src_addr = self.sockfd.recvfrom(1024)
 			recv_data = recv_data.decode('utf-8')
 
 			if recv_data == 'ready':
-				self.addr_list.append(addr)
-				send_data = str(addr)
-				self.sockfd.sendto(send_data.encode('utf-8'), addr)
+				self.addr_list.append(src_addr)
+				send_data = str(src_addr)
+				self.sockfd.sendto(send_data.encode('utf-8'), src_addr)
+				print('list update: %s is added to list' % send_data)
 
-			elif recv_data == 'request' and len(self.addr_list) > 1:
-				n = self.addr_list.index(addr)
+			elif recv_data == 'close':
+				self.addr_list.remove(str(src_addr))
+
+			elif recv_data == 'request' and len(self.addr_list) == 2:
+				n = self.addr_list.index(src_addr)
 				target_addr = self.addr_list[1-n]
 				send_data = str(target_addr)
-				self.sockfd.sendto(send_data.encode('utf-8'), addr)
+				self.sockfd.sendto(send_data.encode('utf-8'), src_addr)
+				print('send %s to %s' % (send_data, str(src_addr)))
 
 			elif recv_data == 'request' and len(self.addr_list) <= 1:
 				send_data = 'none'
-				self.sockfd.sendto(send_data.encode('utf-8'), addr)
+				self.sockfd.sendto(send_data.encode('utf-8'), src_addr)
 
 			else:
 				try:
@@ -54,6 +63,7 @@ class Server():
 					target_addr = self.addr_convert(addr)
 					data = 'addr:' + addr + 'msg:' + msg
 					self.sockfd.sendto(data.encode('utf-8'), target_addr)
+					print('relay message from %s to %s' % (str(src_addr), str(target_addr)))
 				except Exception as e:
 					print('error')
 					pass
